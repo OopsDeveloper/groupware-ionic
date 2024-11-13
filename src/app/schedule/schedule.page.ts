ae6fbe2f8d239521894e6c0b629f2e406a2e6cdc
gys <gys@ibdata.co.kr>
2024-11-13 14:25:47 +0900

    service commit

--- src/app/schedule/schedule.page.ts
+++ src/app/schedule/schedule.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Schedule } from './schedule.model';
-
+import { ScheduleService } from './schedule.service';
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.page.html',
